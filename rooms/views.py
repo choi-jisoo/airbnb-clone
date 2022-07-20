011bf95b6f2ffb82e2d39b03fef60ac25434e18f
Jisoo Choi <jschoi.py@gmail.com>
2022-07-21 01:15:20 +0900

    Add search view - form

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,7 @@
 from django.views.generic.list import ListView
 from django.http import Http404
 from django.shortcuts import render
+from django_countries import countries
 from . import models
 
 
@@ -21,3 +22,51 @@ def room_detail(request, pk):
         return render(request, "rooms/detail.html", context={"room": room})
     except models.Room.DoesNotExist:
         raise Http404()
+
+
+def search(request):
+    city = request.GET.get("city", "Anywhere")
+    city = str.capitalize(city)
+    country = request.GET.get("country", "KR")
+    room_type = int(request.GET.get("room_type", 0))
+    price = int(request.GET.get("price", 0))
+    guests = int(request.GET.get("guests", 0))
+    beds = int(request.GET.get("beds", 0))
+    bedrooms = int(request.GET.get("bedrooms", 0))
+    baths = int(request.GET.get("baths", 0))
+    instant = request.GET.get("instant", False)
+    super_host = request.GET.get("super_host", False)
+    selected_amenities = request.GET.getlist("amenities")
+    selected_facilities = request.GET.getlist("facilities")
+
+    form = {
+        "city": city,
+        "selected_country": country,
+        "selected_room_type": room_type,
+        "price": price,
+        "guests": guests,
+        "beds": beds,
+        "bedrooms": bedrooms,
+        "baths": baths,
+        "instant": instant,
+        "super_host": super_host,
+        "selected_amenities": selected_amenities,
+        "selected_facilities": selected_facilities,
+    }
+
+    room_types = models.RoomType.objects.all()
+    amenities = models.Amenity.objects.all()
+    facilities = models.Facility.objects.all()
+
+    choices = {
+        "countries": countries,
+        "room_types": room_types,
+        "amenities": amenities,
+        "facilities": facilities,
+    }
+
+    return render(
+        request,
+        "rooms/search.html",
+        {**form, **choices},
+    )
