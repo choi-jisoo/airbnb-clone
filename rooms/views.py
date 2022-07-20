4e040ddd47f30c0debf08c5f153bdf07e561e784
Jisoo Choi <jschoi.py@gmail.com>
2022-07-21 02:03:12 +0900

    Add search view - filter

--- rooms/views.py
+++ rooms/views.py
@@ -34,8 +34,8 @@ def search(request):
     beds = int(request.GET.get("beds", 0))
     bedrooms = int(request.GET.get("bedrooms", 0))
     baths = int(request.GET.get("baths", 0))
-    instant = request.GET.get("instant", False)
-    super_host = request.GET.get("super_host", False)
+    instant = bool(request.GET.get("instant", False))
+    superhost = bool(request.GET.get("superhost", False))
     selected_amenities = request.GET.getlist("amenities")
     selected_facilities = request.GET.getlist("facilities")
 
@@ -49,7 +49,7 @@ def search(request):
         "bedrooms": bedrooms,
         "baths": baths,
         "instant": instant,
-        "super_host": super_host,
+        "superhost": superhost,
         "selected_amenities": selected_amenities,
         "selected_facilities": selected_facilities,
     }
@@ -65,8 +65,49 @@ def search(request):
         "facilities": facilities,
     }
 
+    filter_args = {}
+
+    if city != "Anywhere":
+        filter_args["city__startswith"] = city
+
+    filter_args["country"] = country
+
+    if room_type != 0:
+        filter_args["room_type__pk"] = room_type
+
+    if price != 0:
+        filter_args["price__lte"] = price
+
+    if guests != 0:
+        filter_args["guests__gte"] = guests
+
+    if beds != 0:
+        filter_args["beds__gte"] = beds
+
+    if bedrooms != 0:
+        filter_args["bedrooms__gte"] = bedrooms
+
+    if baths != 0:
+        filter_args["baths__gte"] = baths
+
+    if instant is True:
+        filter_args["instant_book"] = True
+
+    if superhost is True:
+        filter_args["host__superhost"] = True
+
+    if len(selected_amenities) > 0:
+        for s_a in selected_amenities:
+            filter_args["amenities__pk"] = int(s_a)
+
+    if len(selected_facilities) > 0:
+        for s_f in selected_facilities:
+            filter_args["facilities__pk"] = int(s_f)
+
+    rooms = models.Room.objects.filter(**filter_args)
+
     return render(
         request,
         "rooms/search.html",
-        {**form, **choices},
+        {**form, **choices, "rooms": rooms},
     )
