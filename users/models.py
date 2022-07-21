ab20befb84418406c51bd569f518297a2b9696ee
Jisoo Choi <jschoi.py@gmail.com>
2022-07-21 14:25:41 +0900

    Add sign up form

--- users/models.py
+++ users/models.py
@@ -37,8 +37,12 @@ class User(AbstractUser):
     gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
     bio = models.TextField(blank=True)
     birthdate = models.DateField(blank=True, null=True)
-    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, blank=True)
-    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, blank=True)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICES, max_length=2, blank=True, default=LANGUAGE_JAPANESE
+    )
+    currency = models.CharField(
+        choices=CURRENCY_CHOICES, max_length=3, blank=True, default=CURRENCY_JPY
+    )
     superhost = models.BooleanField(default=False)
 
     email = models.EmailField(max_length=150, blank=True, unique=True)
